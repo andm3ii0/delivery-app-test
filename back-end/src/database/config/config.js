42f66b3fc0b6b84badbe438381051e6303e3e342
Anderson Mello <91663226+andm3ii0@users.noreply.github.com>
2023-02-03 18:31:11 -0300

    Update config.js

--- back-end/src/database/config/config.js
+++ back-end/src/database/config/config.js
@@ -1,37 +1,16 @@
 require('dotenv').config();
 
-const environment = process.env.NODE_ENV || "test";
-
-const suffix = {
-  prod: "",
-  production: "",
-  dev: "-dev",
-  development: "-dev",
-  test: "-test",
-};
-
-const options = {
-  host: process.env.HOSTNAME || process.env.MYSQL_HOST || process.env.MYSQLHOST || 'localhost',
-  port: process.env.MYSQL_PORT || process.env.MYSQLPORT || '330',
-  database: process.env.MYSQLDATABASE || 
-    `${process.env.MYSQL_DB_NAME ||  'delivery-app'}${suffix[environment] || suffix.test}`,
-  username: process.env.MYSQL_USER || process.env.MYSQLUSER || 'root',
-  password: process.env.MYSQL_PASSWORD || process.env.MYSQLPASSWORD || 'password',
+const config = {
+  username: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  host: process.env.MYSQLHOST,
+  port: process.env.MYSQLPORT,
   dialect: 'mysql',
-  dialectOptions: {
-    timezone: 'Z',
-  },
-  logging: false,
 };
 
 module.exports = {
-  development: {
-    ...options,
-  },
-  test: {
-    ...options,
-  },
-  production: {
-    ...options,
-  },
+  development: config,
+  test: config,
+  production: config,
 };
