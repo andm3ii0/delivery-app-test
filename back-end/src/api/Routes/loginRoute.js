0bcae8fff9055ada82789d17176a2734b143b81d
Anderson Mello <91663226+andm3ii0@users.noreply.github.com>
2023-02-06 02:15:47 -0300

    Delete back-end directory

--- back-end/src/api/Routes/loginRoute.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require('express');
-const loginController = require('../Controller/loginController');
-
-const router = Router();
-
-router.post('/', (req, res) => loginController.login(req, res));
-
-module.exports = router;
--- back-end/src/api/Routes/userRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { getSellers, getUserByEmail } = require('../Controller/userController');
-
-const router = express.Router();
-
-router.get('/sellers', getSellers);
-router.post('/email', getUserByEmail);
-
-module.exports = router;
